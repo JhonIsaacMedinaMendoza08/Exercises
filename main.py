47e227f4425933a0b4f1955fb48675f42b3d4a0e
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-11-11 20:22:51 -0500

    Exercise 21

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
-#Write a program that converts a numeric grade to a letter grade according to a
-#specific grading system, using match .
+#Write a program that calculates the cost of parking based on the number of hours, with progressive rates.
 
 print(""" 
 ---------------------------------------------------------
------------------Exercise 20 ----------------------------
+-----------------Exercise 21 ----------------------------
 ---------------------------------------------------------""")
 
-def convert_grades():
-    grades = float(input("Enter the numerical rating (0-100): "))
-    if not (0 <= grades <= 100):
-        print("The rating must be between 0 and 100.")
-        return
-    match grades:
-        case grades if 90 <= grades <= 100:
-            letter = 'A'
-        case grades if 80 <= grades < 90:
-            letter = 'B'
-        case grades if 70 <= grades < 80:
-            letter = 'C'
-        case grades if 60 <= grades < 70:
-            letter = 'D'
-        case _:
-            letter = 'F'
-    print(f"The letter grade is: {letter}")
-convert_grades()
+hours = int(input("Enter the number of parking hours: "))
+
+cost = 0
+if hours <= 0:
+    print("The number of hours cannot be negative.")
+elif hours <= 1:
+    cost = 5
+elif hours >= 2 and hours <=4:
+    cost = 5 + (hours - 1) * 4
+else:
+    cost = 5 + 3 * 3 + (hours - 4) * 3 
+
+print(f"The total cost of parking is: ${cost}")
