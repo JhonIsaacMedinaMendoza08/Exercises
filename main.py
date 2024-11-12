900d9aedd6823633829478ee6a456b5e0e09384b
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-11-11 19:25:49 -0500

    Exercise 15

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+#Write a program that calculates an employee's net salary after applying taxes.
+
 print (""" 
 ---------------------------------------------------------
---------------------Exercise 14 -------------------------
+-----------------Exercise 15 Salary----------------------
 ---------------------------------------------------------""")
 
-def guess_the_letter():
-    secretletter = "j"
-    while True:
-        letter = input("Guess the secret letter: ").lower()
-        match letter:
-            case "j":
-                print ("Congratulations! You found the secret letter")
-                break
-            case _:
-                print ("Sorry, that's not the secret letter. Please try again.")
-guess_the_letter()
+def salarioNeto ():
+    grossSalary = float(input("Enter your salary before taxes: "))
+    country = input("Enter your country (Country A, Country B, Country C): ")
+    match country.lower():
+        case "a" | "country a" :
+            taxes = 0.20
+        case "b" | "country b":
+            taxes = 0.15
+        case "c" | "country c":
+            taxes = 0.10
+        case _:
+            taxes = 0.25
+        
+    finalTaxes = grossSalary * taxes
+    finalSalary = grossSalary - finalTaxes
+
+    print (f"Your final salary is : {finalSalary}")
+
+salarioNeto()
