27d90c0cfcfa3feec6fad3e7da007a65e4bdac13
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-11-11 19:17:29 -0500

    Exercise 3

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-#Write a program that determines whether a numerical note is "Pass" or "Fail" using if
-print ("Exercise 2 Pass or fail")
+#Use match to implement a simple calculator.
 
-number1 = int(input("Please, enter the score to determine if you pass or fail: "))
-
-if number1 >= 60 :
-    print ("pass")
-else:
-    print ("fail")
+print (""" 
+---------------------------------------------
+--------Exercise 3 Simple calculator---------
+---------------------------------------------""")
+num1 = float(input("    Please enter the number: "))
+symbol = str(input("    Enter the symbol (+, -, *, /): "))
+num2 = float(input("    Please enter the number: "))
+match symbol:
+    case '+':
+        resultado = num1 + num2
+    case '-':
+        resultado = num1 - num2
+    case '*':
+        resultado = num1 * num2
+    case '/':
+        if num2 == 0:
+            print("Error: Cannot divide by zero.")
+        resultado = num1 / num2
+    case _:
+        print("Invalid operation. Choose from ' + ', ' - ', ' * ' or ' / '.")            
+print(f"""
+---------------------------------------------
+The result of the {symbol} is: {resultado}
