df1da409e51f80a6f8326ba8199cc31e5f5740ab
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-11-11 20:39:40 -0500

    Exercise 22

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-#Write a program that calculates the cost of parking based on the number of hours, with progressive rates.
+#Write a program that classifies a triangle as acute, obtuse or right triangle according to its internal angles using if .
 
 print(""" 
 ---------------------------------------------------------
------------------Exercise 21 ----------------------------
+-----------------Exercise 22 ----------------------------
 ---------------------------------------------------------""")
 
-hours = int(input("Enter the number of parking hours: "))
-
-cost = 0
-if hours <= 0:
-    print("The number of hours cannot be negative.")
-elif hours <= 1:
-    cost = 5
-elif hours >= 2 and hours <=4:
-    cost = 5 + (hours - 1) * 4
-else:
-    cost = 5 + 3 * 3 + (hours - 4) * 3 
-
-print(f"The total cost of parking is: ${cost}")
+def triangles():
+    a1 = float(input("Enter the first angle (in degrees): "))
+    a2 = float(input("Enter the second angle (in degrees): "))
+    a3 = float(input("Enter the third angle (in degrees): "))
+    if a1 + a2 + a3 != 180:
+        print("The angles do not add up to 180 degrees. It is not a valid triangle.")
+        return
+    if a1 == 90 or a2 == 90 or a3 == 90:
+        type = "Rectangle"
+    elif a1 > 90 or a2 > 90 or a3 > 90:
+        type = "Obtuss"
+    else:
+        type = "Acute"
+    print(f"The type of triangle is: {type}")
+triangles()
