04e562275ec91dd075725865685388f05affcc23
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-11-11 21:10:30 -0500

    Exercise 6 Iterative Structures

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-#Write a program that prompts the user for two integers, a start value and an end value. The program should print all the 
-# even numbers in that range, including the limits. Use a for loop to iterate through the range.
+#Write a program that generates a random number between 1 and 100 and allows the user to guess it. 
+# The program should give clues as to whether the entered number is greater or less than the secret number.
+
+import random
 print(""" 
 ---------------------------------------------------------
----------Exercise 4 Iterative Structures-----------------
+---------Exercise 6 Iterative Structures-----------------
 ---------------------------------------------------------""")
 
-start = int(input("Enter the start value: "))
-end = int(input("Enter the end value: "))
-
-if start > end:
-    print("The start value must be less than or equal to the end value.")
-
-for even in range(start, end + 1):
-    if even % 2 == 0:
-        print(even)
+def guess_the_number():  
+    number_to_guess = random.randint(1,100)
+    print ("\tWelcome to guess the number")
+    print ("\tthink of a number between 1 and 100")
+    while True:
+        guess = int(input("Enter the number you thought of: "))
+        #print ({number_to_guess})
+        if guess > number_to_guess:
+            print ("The number entered is higher, please try again (numbers between 1-100)")
+        elif guess < number_to_guess:
+            print ("The number entered is less, please try again (numbers between 1-100)")
+        else:
+            print ("the number entered is the same, congratulations!!")
+            break
+guess_the_number()
