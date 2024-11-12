2a6b6230e2d07e3894630cec7576be9ba21dbead
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-11-11 20:45:53 -0500

    Exercise 1 Iterative Structures

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-#Write a program that classifies a triangle as acute, obtuse or right triangle according to its internal angles using if .
+#Write a program that prompts the user for a positive integer n and calculates the sum of the 
+# first n integers. Use a for loop to perform the sum.
 
 print(""" 
 ---------------------------------------------------------
------------------Exercise 22 ----------------------------
+---------Exercise 1 Iterative Structures-----------------
 ---------------------------------------------------------""")
 
-def triangles():
-    a1 = float(input("Enter the first angle (in degrees): "))
-    a2 = float(input("Enter the second angle (in degrees): "))
-    a3 = float(input("Enter the third angle (in degrees): "))
-    if a1 + a2 + a3 != 180:
-        print("The angles do not add up to 180 degrees. It is not a valid triangle.")
+def sum_of_numbers():
+    number = int(input("Enter a positive integer: "))
+
+    if number <= 0:
+        print("Please enter a positive integer.")
         return
-    if a1 == 90 or a2 == 90 or a3 == 90:
-        type = "Rectangle"
-    elif a1 > 90 or a2 > 90 or a3 > 90:
-        type = "Obtuss"
-    else:
-        type = "Acute"
-    print(f"The type of triangle is: {type}")
-triangles()
+    sum = 0
+
+    for i in range(1, number + 1):
+        sum = i + sum
+    print(f"The sum of the first {number} integers is: {sum}")
+sum_of_numbers()
