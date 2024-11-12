c4cf4a80340b5e28e931012bb29af2ee98bf39b2
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-11-11 19:25:01 -0500

    Exercise 13

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-#Write a program that calculates BMI and determines weight status.
-import math
-
+#Write a program that determines the largest of three numbers using if 
 print (""" 
 ---------------------------------------------------------
---------------------Exercise 12 BMI ---------------------
+--------------------Exercise 13 -------------------------
 ---------------------------------------------------------""")
 
-weight = float(input("PLease enter your weight (KG): "))
-height = float(input("Please enter your height (M): "))
+num1 = int(input("Please enter the number 1: "))
+num2 = int(input("Please enter the number 2: "))
+num3 = int(input("Please enter the number 3: "))
 
-imc = weight / math.pow (height, 2)
-
-
-if imc < 18.5:
-    print (f"you are underweight with BMI {imc:.2f}")
-elif imc > 18.5 and imc < 24.9:
-    print (f"You are a normal weight with BMI {imc:.2f}")
-elif imc > 25 and imc < 29.9:
-    print (f"you are overweight with BMI {imc:.2f}")
+if num1 >= num2 and num1 >= num3:
+    print (f"the largest number is: {num1}")
+elif num2 >= num1 and num2 >= num3:
+    print (f"the largest number is: {num2}")
 else:
-    print (f"you are in obesity with BMI {imc:.2f}")
+    print (f"the largest number is: {num3}")
