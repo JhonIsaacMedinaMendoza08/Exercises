368358824fdefa10916c85961f8b077546ed1d04
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-11-11 19:19:52 -0500

    Exercise 7

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
-#Write a program that implements a number guessing game.
-
-import random
+#Write a program that determines whether a number is positive, negative, or zero using if .
 print (""" 
 ---------------------------------------------------------
---------------Exercise 6 guess the number----------------
+-----Exercise 7 Positive, negative or zero number--------
 ---------------------------------------------------------""")
+number = int(input("Enter the number to validate: "))
 
-def guess_the_number():  
-    number_to_guess = random.randint(1,10)
-    print ("\tWelcome to guess the number")
-    print ("\tthink of a number between 1 and 10")
-    while True:
-        guess = int(input("Enter the number you thought of: "))
-        #print ({number_to_guess})
-        if guess > number_to_guess:
-            print ("The number entered is higher, please try again (numbers between 1-10)")
-        elif guess < number_to_guess:
-            print ("The number entered is less, please try again (numbers between 1-10)")
-        else:
-            print ("the number entered is the same, congratulations!!")
-            break
-guess_the_number()
+if number == 0:
+    print ("The number provided is zero")
+elif number < 0:
+    print ("The number provided is negative")
+else:
+    print ("The number provided is positive")
