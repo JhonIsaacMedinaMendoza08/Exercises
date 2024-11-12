553081a9b1f207f94d890f2162b9e96dcee9e073
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-11-11 20:51:56 -0500

    Exercise 2 Iterative Structures

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-#Write a program that prompts the user for a positive integer n and calculates the sum of the 
-# first n integers. Use a for loop to perform the sum.
-
+#Write a program that prompts the user for a string of text and counts how many vowels (a, e, i, o, u) it contains. 
+# Use a for loop to iterate through the string and perform the count.
 print(""" 
 ---------------------------------------------------------
----------Exercise 1 Iterative Structures-----------------
+---------Exercise 2 Iterative Structures-----------------
 ---------------------------------------------------------""")
 
-def sum_of_numbers():
-    number = int(input("Enter a positive integer: "))
-
-    if number <= 0:
-        print("Please enter a positive integer.")
-        return
-    sum = 0
-
-    for i in range(1, number + 1):
-        sum = i + sum
-    print(f"The sum of the first {number} integers is: {sum}")
-sum_of_numbers()
+text = input("Enter a text string: ")
+number_of_vowels = 0
+vowels = "aeiouAEIOU"
+for i in text:
+    if i in vowels: 
+        number_of_vowels += 1
+print(f"The number of vowels in the chain is: {number_of_vowels}")
