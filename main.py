6e5bb447fc5d1b6c272c6b8ecdc0283333ce2bf9
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-11-11 20:02:36 -0500

    Exercise 18

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 #Write a program that calculates a student's final grade based on his/her grade.
 
-print (""" 
+print(""" 
 ---------------------------------------------------------
------------------Exercise 17 ----------------------------
+-----------------Exercise 18 ----------------------------
 ---------------------------------------------------------""")
 
-def final_qualification():
-    qualification = float(input("Enter the student's grade (0-100): "))
-    additionalPoints = input("Did the student do additional homework? (yes/no): ").lower()
-    if additionalPoints == "yes":
-        qualification += (qualification * 0.05)
-        if qualification > 100:
-            qualification = 100  
-    print(f"The student's final grade is: {qualification:.2f}")
+def credits_calculate():
+    num_subjects = int(input("Enter the number of subjects taken: "))
+    total_credits = 0
 
-final_qualification()
+    for i in range(num_subjects):
+        score = float(input(f"Enter the score obtained in subject {i+1}: "))
+        if score >= 60:
+            total_credits += 3  
+    print(f"The total number of credits approved is: {total_credits}")
+credits_calculate()
