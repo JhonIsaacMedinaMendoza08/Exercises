6c4e2adbe54b7d5767d370fe4ff9b0e8a9264c7c
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-11-11 20:12:40 -0500

    Exercise 20

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-#Write a program that calculates a student's final grade based on his/her grade.
+#Write a program that converts a numeric grade to a letter grade according to a
+#specific grading system, using match .
 
 print(""" 
 ---------------------------------------------------------
------------------Exercise 18 ----------------------------
+-----------------Exercise 20 ----------------------------
 ---------------------------------------------------------""")
 
-def credits_calculate():
-    num_subjects = int(input("Enter the number of subjects taken: "))
-    total_credits = 0
-
-    for i in range(num_subjects):
-        score = float(input(f"Enter the score obtained in subject {i+1}: "))
-        if score >= 60:
-            total_credits += 3  
-    print(f"The total number of credits approved is: {total_credits}")
-credits_calculate()
+def convert_grades():
+    grades = float(input("Enter the numerical rating (0-100): "))
+    if not (0 <= grades <= 100):
+        print("The rating must be between 0 and 100.")
+        return
+    match grades:
+        case grades if 90 <= grades <= 100:
+            letter = 'A'
+        case grades if 80 <= grades < 90:
+            letter = 'B'
+        case grades if 70 <= grades < 80:
+            letter = 'C'
+        case grades if 60 <= grades < 70:
+            letter = 'D'
+        case _:
+            letter = 'F'
+    print(f"The letter grade is: {letter}")
+convert_grades()
